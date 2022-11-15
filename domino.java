288dbaab28f7069c6f2ad4e39b30414c703380d6
Tewodros Nibret Adal <99950601+tezena@users.noreply.github.com>
2022-11-15 12:03:43 -0800

    Create domino.java

---
+++ domino.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Domino {
+    
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        
+        
+   int N=scanner.nextInt();
+   int M=scanner.nextInt();
+        System.out.println((N*M)/2);
+    }
+}
