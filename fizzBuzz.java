975616b59d3b335a582a409c4191efa55972034a
Tewodros Nibret Adal <99950601+tezena@users.noreply.github.com>
2022-11-16 23:12:15 -0800

    Update fizzBuzz.java

--- fizzBuzz.java
+++ fizzBuzz.java
@@ -1,59 +1,17 @@
 static public List<String> fizzBuzz(int n) {
-        List<String> list=new ArrayList<String>();
 
-        if(n%3==0 && n%5==0){
-            for(int i=0;i+1<=n;i++){
-                if((i+1<=n && (i+1)%3==0) && (i+1<=n && (i+1)%5==0) ){
+            List<String> list = new ArrayList<String>();
+
+            for (int i = 1; i <= n; i++) {
+                if (i % 3 == 0 && i % 5 == 0) {
                     list.add("FizzBuzz");
-                } else if((i+1)%3==0 ){
+                } else if(i % 3 == 0) {
                     list.add("Fizz");
-                } else if ((i+1)%5==0) {
+                } else if (i % 5 == 0) {
                     list.add("Buzz");
+                } else {
+                    list.add(Integer.toString(i));
                 }
-                else {
-                    list.add(Integer.toString(i + 1));
-                }
-
-
-
             }
+            return list;
         }
-        else if(n%3==0){
-            for(int i=0;i+1<=n;i++){
-                if((i+1<=n && (i+1)%3==0) && (i+1<=n && (i+1)%5==0) ){
-                    list.add("FizzBuzz");
-                }
-                else if(i+1<=n && (i+1)%3==0) {
-                    list.add("Fizz");
-                }else if((i+1)%5==0){
-                    list.add("Buzz");
-                }
-                else {
-                    list.add(Integer.toString(i + 1));
-                }
-
-            }
-
-        } else if (n%5==0) {
-            for(int i=0;i+1<=n;i++){
-                if((i+1<=n && (i+1)%3==0) && (i+1<=n && (i+1)%5==0) ){
-                    list.add("FizzBuzz");
-                }
-                else if(i+1<=n && (i+1)%5==0) {
-                    list.add("Buzz");
-                }else if((i+1)%3==0){
-                    list.add("Fizz");
-                }
-                else {
-                    list.add(Integer.toString(i + 1));
-                }
-
-            }
-        }
-        else{
-            list.add(Integer.toString(n));
-        }
-
-        return list;
-
-    }
