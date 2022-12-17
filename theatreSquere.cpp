b4956ba10a8d5bd3cafd5cda30be0f53b9244b8a
Tewodros Nibret Adal <99950601+tezena@users.noreply.github.com>
2022-12-17 11:45:02 -0800

    Create theatreSquere.cpp

---
+++ theatreSquere.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    double m,n,a;
+    cin >> m>>n >>a;
+    long long x=ceil(m/a);
+    long long y=ceil(n/a);
+
+    cout<<x*y<<endl;
+
+   
+}
