bdcc930a2eeecd30d039c2f3dd330c44f0a19c2c
Tewodros Nibret Adal <99950601+tezena@users.noreply.github.com>
2023-01-22 11:07:45 -0800

    Create RemoveDuplication.cpp

---
+++ RemoveDuplication.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        
+       if(head==NULL || head->next==NULL){
+            return head;
+        }
+        ListNode *temp1=head;
+        ListNode *temp2=head->next;
+
+        while(temp2!=NULL ){
+                if(temp1->val==temp2->val){
+                    temp2=temp2->next;
+                    temp1->next=temp2;
+                }
+                if(temp2!=NULL && temp1->val!=temp2->val){
+                        temp2=temp2->next;
+                       temp1=temp1->next;
+                }
+        
+      
+    }
+      return head;
+    }
+};
