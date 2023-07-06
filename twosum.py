dd5f9447fe94d2d00306b316229e539d92d7c68d
Tewodros Nibret Adal <99950601+tezena@users.noreply.github.com>
2023-07-06 10:18:02 -0700

    Update twosum.py

--- twosum.py
+++ twosum.py
@@ -1,10 +1,6 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+        
         list1=list()
         n=len(nums)
         for i in range(n):
