b05beb66a5df7849a78b425bd7fb3427ba42abb2
Tewodros Nibret Adal <99950601+tezena@users.noreply.github.com>
2023-07-07 12:50:50 -0700

    Update missingNumber.py

--- missingNumber.py
+++ missingNumber.py
@@ -1,9 +1,6 @@
 class Solution(object):
     def missingNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+        
         n=len(nums)
         for x in range(n+1):
             #  print(x)
