6c18ffd85c9df201bf7b7a5b48dada114856d7b4
Tewodros Nibret Adal <99950601+tezena@users.noreply.github.com>
2023-07-08 12:53:00 -0700

    Update Valid Anagram.py

--- Valid Anagram.py
+++ Valid Anagram.py
@@ -1,10 +1,6 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
+       
         if Counter(s) == Counter(t):
            set1=set(s)
            set2=set(t)
